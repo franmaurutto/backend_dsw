0baa6e6a8673682615a1beb9c90735ccf1daf1e5
Delfina Mansilla <delfimansilla.ros@gmail.com>
2024-11-07 13:58:15 -0300

    cambio en parcial

--- src/parcial/parcial.controler.ts
+++ src/parcial/parcial.controler.ts
@@ -44,15 +44,14 @@ async function findOne(req: Request, res: Response){
       res.status(500).json({ message: error.message })
     }}
 
-/*async function add(req: Request, res: Response) {
-  console.log(`parcial add req.body: ${JSON.stringify(req.body.sanitizedInput)}`);
+async function add(req: Request, res: Response) {
+console.log(`parcial add req.body: ${JSON.stringify(req.body.sanitizedInput)}`);
   try {
     const curso = await em.findOne(Curso, { id: req.body.sanitizedInput.cursoId });
+    const inscripcion = await em.findOne(Inscripcion, { id: req.body.sanitizedInput.inscripcionId });
     if (!curso) {
       return res.status(404).json({ message: 'Curso no encontrado' });
-    } }
- /* try {
-    const inscripcion = await em.findOne(Inscripcion, { id: req.body.sanitizedInput.inscripcionId });
+    const inscripcion = await em.findOne(Inscripcion, { id: req.body.sanitizedInput.inscripcionId })};
     if (!inscripcion) {
       return res.status(404).json({ message: 'inscripcion no encontrada' });
     }
@@ -60,14 +59,14 @@ async function findOne(req: Request, res: Response){
       ...req.body.sanitizedInput,
       curso,        
       inscripcion,      
-    });*/
-    /*await em.persistAndFlush(parcial);
+    });
+    await em.persistAndFlush(parcial);
     res.status(201).json({ message: 'Parcial ha sido creado', data: parcial });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
-  } *///}
-    //no me funciona*/
-    async function add(req: Request, res: Response){
+  } }
+  
+    /*async function add(req: Request, res: Response){
       try {
         const parcial = em.create(Parcial, req.body)
         await em.flush()
@@ -75,7 +74,7 @@ async function findOne(req: Request, res: Response){
       } catch (error: any) {
         res.status(500).json({ message: error.message })
       }
-    }
+    }*/
 async function update(req: Request, res: Response){
     try {
       const id = Number.parseInt(req.params.id)
