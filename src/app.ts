b57eeb425ba26a7053b86ef0507331dae3bb5827
Francesca Maurutto <franchuls666@gmail.com>
2024-12-16 17:58:27 -0300

    cambios en el app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app=getApp()
 const PORT = process.env.PORT || 3000;
-
-await syncSchema();
-
-app.listen(PORT, () => {
-    console.log(`Server running on ${process.env.API_SERVER || 'http://localhost'}:${PORT}`);
-});
+const startServer = async () => {
+    try {
+      // Sincronizar el esquema de la base de datos
+      await syncSchema();
+      console.log('Esquema de la base de datos sincronizado exitosamente.');
+  
+      // Iniciar el servidor
+      app.listen(PORT, () => {
+        console.log(`Server running on ${process.env.API_SERVER || 'http://localhost'}:${PORT}`);
+      });
+    } catch (error) {
+      console.error('Error al sincronizar el esquema de la base de datos:', error);
+    }
+  };
+  
+  startServer();
